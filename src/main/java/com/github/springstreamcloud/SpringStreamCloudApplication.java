61d47280a3b5c7d94be52842c726b4c327576506
Hazar Gul Nazari <hazar.nazari@favendo.com>
2020-03-31 19:08:26 +0200

    first commit

---
+++ src/main/java/com/github/springstreamcloud/SpringStreamCloudApplication.java
@@ -0,0 +1,13 @@
+package com.github.springstreamcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringStreamCloudApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringStreamCloudApplication.class, args);
+	}
+
+}
